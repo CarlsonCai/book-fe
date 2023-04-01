2ecb56b424fc9f4f0866cb0de3213c58bd869408
Carlson_Tsai <carlson_tsai@paradise-soft.com.tw>
2023-04-01 19:25:20 +0800

    .

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createWebHistory, createRouter, RouteRecordRaw } from 'vue-router'
 export const constantRoutes: RouteRecordRaw[] = [
   {
     name: 'Login',
-    path: '/login',
+    path: '/',
     component: () => import('../views/login/Login.vue'),
     meta: {
       title: '登录',
